2f9327b5aa3162af4134c5a02010ae738d67979c
amirthapamagar2053 <amirthapa2019@gmail.com>
2022-08-31 13:22:42 +0545

    feat:will connect the BE to the MONGO

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const Person = require("./models/phonebook");
 const App = express();
 App.use(express.static("build"));
 App.use(cors());
@@ -8,71 +9,103 @@ App.use(express.json());
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 App.get("/api/persons", (req, res) => {
-  res.send(persons);
+  Person.find({}).then((result) => {
+    console.log("the result is", result);
+    res.send(result);
+  });
+});
+
+App.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  console.log("the person is", person);
+  console.log(request.params.id);
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedNote) => {
+      console.log(updatedNote);
+      response.send(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 App.get("/info", (req, res) => {
   let today = new Date();
   res.setHeader("content-type", "text/plain");
-  res.send(`Phonebook has info for ${persons.length} people
+  res.send(`Phonebook has info for ${Person.length} people
 ${today}`);
 });
 
 App.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  const currentid = persons.find((x) => x.id === id);
-  if (currentid) res.send(currentid);
-  else res.status(404).send("404 Not Found");
+  Person.findById(req.params.id).then((person) => {
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+  });
 });
 
 App.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((x) => x.id !== id);
+  Person.findByIdAndRemove(req.params.id).then((result) => {
+    res.status(204).end();
+  });
 });
 
 App.post(
   "/api/persons",
   morgan(":method :url :status :response-time ms :body"),
   (req, res) => {
+    console.log(req.body);
     let myIncomingData = req.body;
     myIncomingData.id = Math.floor(Math.random() * 60);
-
-    if (
-      !Object.keys(myIncomingData).includes("name") ||
-      !Object.keys(myIncomingData).includes("number") ||
-      persons.map((x) => x.name).includes(myIncomingData.name)
-    )
-      res.send({ error: "name must be unique" });
-    else {
-      persons.push(myIncomingData);
-      res.status(201).json(myIncomingData);
-    }
+    Person.find({ name: myIncomingData.name }).then((result) => {
+      if (result.length === 1) {
+        res.json({ error: "name must be unique" });
+      } else {
+        console.log("the else entered");
+        const person = new Person({
+          name: myIncomingData.name,
+          number: myIncomingData.number,
+          id: myIncomingData.id,
+        });
+        person.save().then((result) => {
+          console.log(result);
+          res.send(result);
+        });
+      }
+    });
   }
 );
+
 const PORT = process.env.PORT || 3001;
 
 App.listen(PORT, () => console.log(`starting the dev server on ${PORT}`));
+
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
